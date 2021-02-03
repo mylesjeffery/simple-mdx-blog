bf7907df07b30309dcb737f806daa3059e24a057
Myles Jeffery <mylesjeffery96@gmail.com>
2021-02-03 11:26:55 -0800

    added content card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="March 2-th, 2020"
+          title="Big TItle"
+          excerpt="excerpt here"
+          slug="/test"
+        />
+      </Content>
     </Container>
   )
 }
